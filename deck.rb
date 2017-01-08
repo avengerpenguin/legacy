fde5d31430757ce7c666bb494be721982ecd253e
AvengerPenguin <github@rossfenning.co.uk>
2017-01-08 23:11:43 +0000

    Added first agenda card

--- deck.rb
+++ deck.rb
@@ -136,3 +136,18 @@ Squib::Deck.new(cards: 1, layout: 'layouts/main.yml') do
 
   save_png prefix: '3-opportunity-'
 end
+
+Squib::Deck.new(cards: 1, layout: 'layouts/main.yml') do
+  background color: 'white'
+  data = csv file: 'data/agendas.csv'
+
+  text str: data['title'], layout: 'Title'
+  text str: 'Agenda', layout: 'Type'
+  svg file: data['icon'], layout: 'Picture'
+
+  rect x: 30, y: 650, width: 765, height: 450, radius: 8
+
+  text str: data['ability_text'], layout: 'RuleText'
+
+  save_png prefix: '3-agenda-'
+end
