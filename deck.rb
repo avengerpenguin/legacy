be0d65f97a805920fbc0e307a9cae4299c899acf
AvengerPenguin <github@rossfenning.co.uk>
2017-01-08 21:20:43 +0000

    Got some examples of some card types

--- deck.rb
+++ deck.rb
@@ -1,29 +1,73 @@
 require 'squib'
 
-Squib::Deck.new(cards: 2, layout: 'layouts/item.yml') do
+Squib::Deck.new(cards: 2, layout: 'layouts/main.yml') do
   background color: 'white'
-  data = csv file: 'data/item.csv'
+  data = csv file: 'data/items.csv'
 
-  text str: data['title'], layout: 'title'
-  svg file: data['icon'],
-      width: 500, height: 500,
-      x: 150, y: 140
+  text str: data['title'], layout: 'Title'
+  text str: 'Item', layout: 'Type'
+  svg file: data['icon'], layout: 'Picture'
 
-  rect x: 20, y: 640, width: 785, height: 465, radius: 8
+  rect x: 30, y: 650, width: 765, height: 450, radius: 8
 
   text str: data['temp_title'], layout: 'Oneshot'
   text str: data['temp_bonus'], layout: 'OneshotBonus'
   svg file: data['temp_type'], layout: 'OneshotBonusType'
 
-  text str: data['perm_title'], layout: 'Fixture'
+  #text str: data['perm_title'], layout: 'Fixture'
   text str: data['perm_bonus'], layout: 'FixtureBonus'
   svg file: data['perm_type'], layout: 'FixtureBonusType'
 
-  save_png prefix: 'fortune_'
-  showcase file: 'showcase_fortune.png', fill_color: '#0000'
+  save_png prefix: '1-item-'
 end
 
-#Squib::Deck.new(cards: 3, layout: 'misfortune-layout.yml') do
-#  text str: 'Hello, World!'
-#  save format: :png
-#end
+Squib::Deck.new(cards: 1, layout: 'layouts/main.yml') do
+  background color: 'white'
+  data = csv file: 'data/events.csv'
+
+  text str: data['title'], layout: 'Title'
+  text str: 'Event', layout: 'Type'
+  svg file: data['icon'], layout: 'Picture'
+
+  rect x: 30, y: 650, width: 765, height: 450, radius: 8
+
+  text str: data['ignore_text'], layout: 'Oneshot'
+
+  text str: data['perm_bonus'], layout: 'FixtureBonus'
+  svg file: data['perm_type'], layout: 'FixtureBonusType'
+
+  save_png prefix: '1-event-'
+end
+
+Squib::Deck.new(cards: 1, layout: 'layouts/main.yml') do
+  background color: 'white'
+  data = csv file: 'data/buildings.csv'
+
+  text str: data['title'], layout: 'Title'
+  text str: 'Building', layout: 'Type'
+  svg file: data['icon'], layout: 'Picture'
+
+  rect x: 30, y: 650, width: 765, height: 450, radius: 8
+
+  text str: data['ability_text'], layout: 'RuleText'
+
+  text str: data['perm_bonus'], layout: 'FixtureBonus'
+  svg file: data['perm_type'], layout: 'FixtureBonusType'
+
+  save_png prefix: '2-building-'
+end
+
+Squib::Deck.new(cards: 1, layout: 'layouts/main.yml') do
+  background color: 'white'
+  data = csv file: 'data/people.csv'
+
+  text str: data['title'], layout: 'Title'
+  text str: 'Person', layout: 'Type'
+  svg file: data['icon'], layout: 'Picture'
+
+  rect x: 30, y: 650, width: 765, height: 450, radius: 8
+
+  text str: data['ability_text'], layout: 'RuleText'
+
+  save_png prefix: '2-person-'
+end
