28fa1e610dc99035fd7c1b1a34b9db665000a87a
AvengerPenguin <github@rossfenning.co.uk>
2017-03-18 09:41:42 +0000

    Building first deck form Google docs

--- deck.rb
+++ deck.rb
@@ -1,15 +1,25 @@
 require 'squib'
+require 'open-uri'
 
-data_items = Squib.csv file: 'data/items.csv'
-#data_events = Squib.csv file: 'data/events.csv'
 
-Squib::Deck.new(cards: data_items.nrows, layout: 'layouts/main.yml') do
+def get_from_google(sheet_gid)
+  google_sheet_id = '1UbQaWcCSOdeHLwQfvljuDZiQwUvRZ1Wjuvg1YPRqyos'
+  buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid}").read
+  Squib.csv data: buffer
+end
+
+icon_size = 30
+
+
+data = get_from_google 0
+
+
+Squib::Deck.new(cards: data.nrows, layout: 'layouts/main.yml') do
   background color: 'white'
-  data = csv file: 'data/items.csv'
 
-  text str: data['title'], layout: 'Title'
-  text str: 'Item', layout: 'Type'
-  svg file: data['icon'], layout: 'Picture'
+  text str: data['Title'], layout: 'Title'
+  text str: data['Type'], layout: 'Type'
+  svg file: data['Icon'].map{|x| "icons/#{x}.svg"}, layout: 'Picture'
 
   bleed = inches(0.125)
   rect x: bleed, y: bleed,
@@ -21,20 +31,33 @@ Squib::Deck.new(cards: data_items.nrows, layout: 'layouts/main.yml') do
 
   rect x: 100, y: 650, width: 625, height: 375, radius: 8
 
-  text str: data['temp_title'], layout: 'Oneshot'
-  text str: data['temp_bonus'], layout: 'OneshotBonus'
-  svg file: data['temp_type'], layout: 'OneshotBonusType'
+  text str: data['One-time Action'], layout: 'Oneshot'
+  text str: data['One-time Bonus'], layout: 'OneshotBonus' do |embed|
+    embed.svg key: 'Strength', file: 'icons/crossed-swords.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Magic', file: 'icons/fairy-wand.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Wealth', file: 'icons/crown-coin.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Shadow', file: 'icons/cowled.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Knowledge', file: 'icons/white-book.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Influence', file: 'icons/public-speaker.svg', width: icon_size, height: icon_size
+  end
 
-  text str: data['perm_title'], layout: 'Fixture'
-  text str: data['perm_bonus'], layout: 'FixtureBonus'
-  svg file: data['perm_type'], layout: 'FixtureBonusType'
+  text str: data['Permanent Action'], layout: 'Fixture'
+  text str: data['Permanent Bonus'], layout: 'FixtureBonus' do |embed|
+    embed.svg key: 'Strength', file: 'icons/crossed-swords.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Magic', file: 'icons/fairy-wand.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Wealth', file: 'icons/crown-coin.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Shadow', file: 'icons/cowled.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Knowledge', file: 'icons/white-book.svg', width: icon_size, height: icon_size
+    embed.svg key: 'Influence', file: 'icons/public-speaker.svg', width: icon_size, height: icon_size
+  end
 
-  save_png prefix: '1-item-', count_format: '%03d'
+  #save_png prefix: '1-item-', count_format: '%03d'
   save_sheet prefix: 'sheet-1-', rows: 4, columns: 4
 end
 
 
 data_growth = Squib.csv file: 'data/deck-2-growth.csv'
+data = get_from_google 1
 
 
 Squib::Deck.new(cards: data_growth.nrows, layout: 'layouts/main.yml') do
@@ -118,7 +141,6 @@ Squib::Deck.new(cards: deck3_size, layout: 'layouts/main.yml') do
       data_legacy['icon'])), layout: 'Picture'
 
 
-  icon_size = 30
   text(str: data_opportunity['ability_text']
                 .concat(data_agenda['ability_text'].concat(
       data_legacy['ability_text'])), layout: 'RuleText') do |embed|
